f37e78f3c87f04e1c65c64554ab4f9ccda1c96e2
mauricio pantoja <mauriciop173@hotmail.com>
2023-05-15 19:37:41 -0500

    Config-Mongo-Database

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-import  Express  from "express";
+import  express  from "express";
 import "dotenv/config.js"
-const server = Express();
-const PORT = process.env.PORT || 8080;
+import "./config/database.js"
 
+const server = express();
+const PORT = process.env.PORT || 8080;
 const ready = () => console.log('Server ready on port : ' + PORT)
+
+//middlewares
+server.use(express.json()) //permite entradas y trabajar con archivos js
+server.use(express.urlencoded({ extended: false }))// permite hacer consultas complejas 
+
+//Router + server
 server.listen(PORT, ready);
+
 console.log(process.env.NODE_ENV)
